8e74c38c61c1276eed3f2c5bd7d8fa878c08399f
@Faimasoft786 <FatimaSoft786@gmail.com>
2024-06-20 17:00:37 +0500

    added

--- Router/Admin.js
+++ Router/Admin.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { signup,login, logout,fetchUserByID,verifyOtp,checkEmail,resetPassword,doctors,patients,doctorsAccounts,countData} = require('../Controller/Admin');
+const { signup,login,fetchUserByID,verifyOtp,checkEmail,resetPassword,doctors,patients,doctorsAccounts,countData} = require('../Controller/Admin');
 var fetchUser = require('../middleware/fetchUser');
 const router = express.Router();
 router.post('/signup', signup)
