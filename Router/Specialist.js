4fbf7849c129bde7082f8c3963a9dc2bc2cde462
@Faimasoft786 <FatimaSoft786@gmail.com>
2024-06-24 15:18:16 +0500

    update the specialist api

--- Router/Specialist.js
+++ Router/Specialist.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const {addSpecialist,getSpecialists,fetchDoctors,fetchAllDoctors} = require("../Controller/Specialist")
 const router = express.Router();
-
+var fetchUser = require('../middleware/fetchUser');
 router.post("/addSpecialist",addSpecialist);
-router.get("/fetchSpecialists",getSpecialists);
+router.get("/fetchSpecialists",fetchUser,getSpecialists);
 router.post("/findDoctor",fetchDoctors);
 router.get("/fetchAll",fetchAllDoctors);
 
