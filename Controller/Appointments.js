0dacf70e18f765eb94a7e7e3bd2f72946f37174b
@Faimasoft786 <FatimaSoft786@gmail.com>
2024-06-27 04:41:35 +0500

    added the default url

--- Controller/Appointments.js
+++ Controller/Appointments.js
@@ -175,7 +175,7 @@ const fetchAppointmentByDoctor = async(req,res)=>{
     if(!data){
       return res.json({success: false, message: "Data not found"})
     }else{
-     const appointments = await Appointment.find().populate('patient', '_id firstName lastName picture_url postal_code sex dob location phoneNumber good_health serious_illness serious_illness_description past_surgery past_surgery_description current_medication current_medication_description heart_disease blood_pressure  allergies allergies_description diabetes kidney_disease thyroid stomach_disease  digestive_disease  digestive_description lung_disease lungs_description venereal nervous hormone any_illness any_illness_description smoke alcohol aids usual_medicine usual_medicine_description');
+     const appointments = await Appointment.find().populate('patient', '_id firstName lastName picture_url default_picture_url postal_code sex dob location phoneNumber good_health serious_illness serious_illness_description past_surgery past_surgery_description current_medication current_medication_description heart_disease blood_pressure  allergies allergies_description diabetes kidney_disease thyroid stomach_disease  digestive_disease  digestive_description lung_disease lungs_description venereal nervous hormone any_illness any_illness_description smoke alcohol aids usual_medicine usual_medicine_description');
     
       const adminAmountsDict = {};
     appointments.forEach(appointment => {
