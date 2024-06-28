04363e6931db25bbba5f848956dfcbfa32ec87e3
@Faimasoft786 <FatimaSoft786@gmail.com>
2024-06-28 21:32:18 +0500

    Medical history api is updated

--- Controller/User.js
+++ Controller/User.js
@@ -397,7 +397,7 @@ const patientMedicalHistory = async(req,res)=>{
         },
         { new: true }
       );
-       res.json({success: true, message: data})
+       res.json({success: true, user_details: data})
         }
         
     } catch (error) {
